4328d83d9a5444c4bce729196a19db109464da37
Maica <maicasantos90@gmail.com>
2022-06-17 13:57:51 -0700

    login modal

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -4,7 +4,7 @@ import LoginForm from './LoginForm';
 
 function LoginFormModal() {
   const [showModal, setShowModal] = useState(false);
-
+  // console.log("Hello from demo")
   return (
     <>
       <button onClick={() => setShowModal(true)}>Log In</button>
