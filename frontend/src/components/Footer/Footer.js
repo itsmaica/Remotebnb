9b77b2c4c9ba35cd6a47560a7fac2a1a60fa811d
Maica <maicasantos90@gmail.com>
2022-06-19 03:17:17 -0700

    splash page done

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,67 @@
+import { Link } from "react-router-dom";
+
+import footer from "../../images/footer.jpeg";
+import linkedin from "../../images/linkedin.png";
+import githubicon from "../../images/githubicon.png";
+
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <>
+      <div className="about-container">
+        <div className="about">
+          <div className="created-by">
+            <h3>Created By Maica Santos</h3>
+          </div>
+
+          <div className="icons-bot">
+            <div className="icon-box">
+              <Link
+                to={{ pathname: "https://www.linkedin.com/in/maicasantos/" }}
+                target="_blank"
+              >
+                <img className="about-icons" src={linkedin} alt="linked" />
+              </Link>
+            </div>
+
+            <div className="icon-box">
+              <Link
+                to={{ pathname: "https://github.com/itsmaica" }}
+                target="_blank"
+              >
+                <img className="about-icons" src={githubicon} alt="github" />
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="technologies">
+          <div id="tech-title">
+            <h4>Technologies Used</h4>
+          </div>
+
+          <div className="lang">
+                <img src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E" />
+                <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" />
+                <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" />
+                <img src="https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white" />
+                <img src="https://img.shields.io/badge/Express.js-000000?style=for-the-badge&logo=express&logoColor=white" />
+                <img src="https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white" />
+                <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" />
+                <img src="https://img.shields.io/badge/Redux-593D88?style=for-the-badge&logo=redux&logoColor=white" />
+                <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" />
+                <img src="https://img.shields.io/badge/Postman-FF6C37?style=for-the-badge&logo=postman&logoColor=white" />
+                <img src="https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white" />
+          </div>
+        </div>
+      </div>
+
+      <div className="test">
+        <div className="filler-div"></div>
+        <img id="foot" src={footer} alt="green footer" />
+      </div>
+    </>
+  );
+}
+export default Footer;
--- frontend/src/components/Splash/Splash.js
+++ frontend/src/components/Splash/Splash.js
@@ -1,9 +1,108 @@
-// import "./Splash.css"
+import "./Splash.css"
+
+import Footer from "../Footer/Footer";
+import nydigitalnomad from "../../images/nydigitalnomad.jpeg"
+import modern from "../../images/modern.jpeg"
 
 function Splash(){
     return(
         <>
-        <h1>Welcome To Remotebnb</h1>
+            <div className="pic-try-hosting">
+                <div id="splash-house">
+                    <img id="nynomad" src={nydigitalnomad} />
+                </div>
+                <div id="splash-host">
+                    <h1>Welcome To Remotebnb</h1>
+                    <div id="about-rem">
+                        <p>Working remotely from home has become the norm for many people in recent times. This shift has allowed better work-life balance, increased productivity, and more freedom. Remotebnb serves as a platform for our users to find unique homes where they can work from.</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            <div className="try-host">
+                <div id="modern">
+                    <img id="mod" src={modern} />
+                </div>
+                <div id="host-ad">
+                    <div>
+                        <h1>Try Hosting A Spot</h1>
+                    </div>
+                    <div id="about-rem">
+                        <p>Users can manage their spots that they would like to share with others and write reviews of other spots. </p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="splash-reviews">
+                <div className="splash-reviews">
+                    <div id="rev-heading">
+                        <h2>What Guests Are Saying</h2>
+                    </div>
+
+                    <div id="splash-revs">
+
+                        <div className="s-rev">
+                            <div className="s-pic-rev">
+                                <img className="s-r-pic" src="https://remotebnb.s3.us-west-1.amazonaws.com/blue.png"/>
+                            </div>
+                            <div className="name-and-pic">
+                                <div className="splash-user">
+                                    <img className="s-user-profile" src="https://remotebnb.s3.us-west-1.amazonaws.com/Patrick_Levis.webp"/>
+                                </div>
+                                <div className="name-cont">
+                                    <h4 className="s-user-name">Jack</h4>
+                                </div>
+                            </div>
+                            <div className="s-rev-box">
+                               <p>"I loved staying at this spot in Boston. The internet was super fast, and the location was really convenient. I would definitely work from here again."</p>
+                            </div>
+                        </div>
+
+                        <div className="s-rev">
+                            <div className="s-pic-rev">
+                                <img className="s-r-pic" src="https://remotebnb.s3.us-west-1.amazonaws.com/soholoft.png"/>
+                            </div>
+                            <div className="name-and-pic">
+                                <div className="splash-user">
+                                    <img className="s-user-profile" src="https://remotebnb.s3.us-west-1.amazonaws.com/Screen+Shot+2022-06-19+at+2.46.13+AM.png"/>
+                                </div>
+                                <div className="name-cont">
+                                    <h4 className="s-user-name">Fiona</h4>
+                                </div>
+                            </div>
+                            <div className="s-rev-box">
+                                <p>"Had an awesome experience at the loft in Soho. After finishing our project we were able to enjoy the downtown area. Would absolutely recommend to anyone."</p>
+                            </div>
+                        </div>
+
+                        <div className="s-rev">
+                            <div className="s-pic-rev">
+                                <img className="s-r-pic" src="https://remotebnb.s3.us-west-1.amazonaws.com/hawaiicabin.png"/>
+                            </div>
+                            <div className="name-and-pic">
+                                <div className="splash-user">
+                                    <img className="s-user-profile" src="https://remotebnb.s3.us-west-1.amazonaws.com/Screen+Shot+2022-06-19+at+2.49.40+AM.png"/>
+                                </div>
+                                <div className="name-cont">
+                                    <h4 className="s-user-name">Clu</h4>
+                                </div>
+                            </div>
+                            <div className="s-rev-box">
+                                <p>"Working remote from Hawaii is literally the best. Nothing beats walking along the beach on your lunch break."</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        <div className="bottom-foot">
+            <Footer/>
+        </div>
         </>
     )
 }
