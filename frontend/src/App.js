7a37d9ef34949ad8308a1408855b99093ab241d5
Maica <maicasantos90@gmail.com>
2022-07-25 08:31:30 -0700

    update files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,8 @@ import SplashSpot from './components/Spots/SplashSpot';
 import Building from './components/LoadingAndPageNotFound/Building';
 import CalendarComponent from './components/Bookings/Calendar/Calendar';
 import DateTimeRangePicker from '@wojtekmaj/react-daterange-picker';
+import DateRange from './components/Bookings/DateRange/DateRange';
+import SideBookings from './components/Bookings/SideBookings/SideBooking';
 // import { getReviewThunk } from './store/review';
 // import Reviews from './components/Reviews/Reviews';
 
@@ -52,7 +54,9 @@ function App() {
           <Route exact path='/test'>
             {/* <Splash /> */}
             {/* <CalendarComponent/> */}
-            <DateTimeRangePicker />
+            {/* <DateTimeRangePicker /> */}
+            {/* <DateRange /> */}
+            <SideBookings />
 
           </Route>
 
--- frontend/src/components/Bookings/DateRange/DateRange.js
+++ frontend/src/components/Bookings/DateRange/DateRange.js
@@ -1,5 +1,9 @@
 import DateRangePicker from '@wojtekmaj/react-daterange-picker'
+
 import { useState } from 'react'
+import "@wojtekmaj/react-daterange-picker/dist/DateRangePicker.css"
+// import DateRangePicker from 'rsuite/DateRangePicker'
+// import 'rsuite/dist/styles/rsuite-default.css';
 
 function DateRange(){
     const [dates, setDates] = useState([new Date(), new Date()]);
@@ -7,11 +11,19 @@ function DateRange(){
 
     return(
         <>
-        <h1>Testing dateRange</h1>
-        <DateRangePicker
-        onChange={setDates}
-        value={dates}
-        />
+        {/* <h1>Testing rsuite</h1> */}
+        <div>
+            <DateRangePicker
+            onChange={setDates}
+            value={dates}
+
+            />
+        </div>
+
+        {/* <div>
+            <h2>TEST</h2>
+            <DateRangePicker block/>
+        </div> */}
         </>
     )
 }
