d4656fc71794f84845e6e483be9e6d6eadc97352
Maica <maicasantos90@gmail.com>
2022-09-16 14:20:37 -0700

    counter completed

--- frontend/src/components/Bookings/ChildGuestTracker/ChildGuestTracker.js
+++ frontend/src/components/Bookings/ChildGuestTracker/ChildGuestTracker.js
@@ -1,64 +1,62 @@
 import { useState } from "react";
 import "./ChildGuestTracker.css";
 
-function ChildGuestTracker({child, setChild}) {
-
-
-
-
-
-    const add =()=>{ setChild(child+1) }
-    const sub=()=>{ setChild(child-1) }
-
+function ChildGuestTracker({ child, setChild, guestNum, adult }) {
+  const add = () => {
+    setChild(child + 1);
+  };
+  const sub = () => {
+    setChild(child - 1);
+  };
 
   return (
     <>
       {/* <p>Maica is the greatest of all time!</p> */}
       <div id="g-count">
         {child === 0 ? (
-        <div
-        id="sub-grey"
-        className="round">
-            { child === 0 ? (
-                 <div id="sub-disable"className="fill">
-                    <i id="dis-i"class="fa-solid fa-minus"></i>
-                </div>
-                ) :
-                <div className="fill">
+          <div id="sub-grey" className="round">
+            {child === 0 ? (
+              <div id="sub-disable" className="fill">
+                <i id="dis-i" class="fa-solid fa-minus"></i>
+              </div>
+            ) : (
+              <div className="fill">
                 <i class="fa-solid fa-minus"></i>
-                </div>
-                }
-        </div>
-        ) :
-        <div
-        onClick={sub}
-        id="sub"
-        className="round">
-            { child === 0 ? (
-                 <div id="sub-disable"className="fill">
-                    <i class="fa-solid fa-minus"></i>
-                </div>
-                ) :
-                <div className="fill">
+              </div>
+            )}
+          </div>
+        ) : (
+          <div onClick={sub} id="sub" className="round">
+            {child === 0 ? (
+              <div id="sub-disable" className="fill">
                 <i class="fa-solid fa-minus"></i>
-                </div>
-                }
-        </div>
-        }
+              </div>
+            ) : (
+              <div className="fill">
+                <i class="fa-solid fa-minus"></i>
+              </div>
+            )}
+          </div>
+        )}
 
         <div id="g-ct-num">
           <p>{child}</p>
         </div>
 
-        <div
-        onClick={add}
-        id="add"
-        className="round">
-            <div className="fill">
-
-          <i class="fa-solid fa-plus"></i>
-            </div>
+        {child + adult !== guestNum ? (
+        <div onClick={add} id="add" className="round">
+          <div className="fill">
+            <i class="fa-solid fa-plus"></i>
+          </div>
         </div>
+
+        ):(
+          <div id="add" className="round-dis">
+          <div className="fill">
+            <i id="dis-add" class="fa-solid fa-plus"></i>
+          </div>
+        </div>
+        )}
       </div>
     </>
   );
